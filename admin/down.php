82fe961c7fd650a9ae21078ce110bba210b6a3b8
Renjie <buoren2006@gmail.com>
2016-04-20 11:28:38 +0800

    Add export form

--- admin/down.php
+++ admin/down.php
@@ -37,7 +37,77 @@
 		$conn = new DBSQL();
 		$sql  = 'select * from user where date(addtime) = "'.$today.'"';
 		$data = $conn->select($sql);
-		dump($data); 
+
+		require_once '../lib/PHPExcel.php';
+
+        $objPHPExcel = new PHPExcel();
+        // 设置excel 列title
+        $objPHPExcel->setActiveSheetIndex(0)
+            ->setCellValue( 'A1','username')
+            ->setCellValue( 'B1','company')
+            ->setCellValue( 'C1','department')
+            ->setCellValue( 'D1','title')
+            ->setCellValue( 'E1','email')
+            ->setCellValue( 'F1','topics')
+            ->setCellValue( 'G1','channel')
+            ->setCellValue( 'H1','event')
+            ->setCellValue( 'I1','other_event')
+            ->setCellValue( 'J1','remark')
+            ->setCellValue( 'K1','other_remark')
+            ->setCellValue( 'L1','addtime');
+
+
+
+        // 设置单元格宽度
+		/*
+        $objPHPExcel->getActiveSheet()->getColumnDimension('A')->setWidth(25);
+        $objPHPExcel->getActiveSheet()->getColumnDimension('B')->setWidth(15);
+        $objPHPExcel->getActiveSheet()->getColumnDimension('C')->setWidth(15);
+        $objPHPExcel->getActiveSheet()->getColumnDimension('D')->setWidth(15);
+        $objPHPExcel->getActiveSheet()->getColumnDimension('G')->setWidth(13);
+        $objPHPExcel->getActiveSheet()->getColumnDimension('E')->setWidth(11);
+        $objPHPExcel->getActiveSheet()->getColumnDimension('H')->setWidth(11);
+		*/
+        $objPHPExcel->getActiveSheet()->getStyle('H')->getFill()->getStartColor()->setARGB('FF808080');
+
+		$num = 1;
+		foreach ($data as $key=>$value) {
+		$num++;
+		  $objPHPExcel->setActiveSheetIndex(0)
+                //Excel的第A列，uid是你查出数组的键值，下面以此类推
+                ->setCellValue('A'.$num,$value['username'])
+                ->setCellValue('B'.$num,$value['company'])
+                ->setCellValue('C'.$num,$value['department'])
+                ->setCellValue('D'.$num,$value['title'])
+                ->setCellValue('E'.$num,$value['email'])
+                ->setCellValue('F'.$num,$value['topics'])
+                ->setCellValue('G'.$num,$value['channel'])
+                ->setCellValue('H'.$num,$value['event'])
+                ->setCellValue('I'.$num,$value['other_event'])
+                ->setCellValue('J'.$num,$value['remark'])
+                ->setCellValue('K'.$num,$value['other_remark'])
+                ->setCellValue('L'.$num,$value['addtime']);
+		}
+
+        
+
+        $objPHPExcel->getActiveSheet()->setTitle('output Form');
+
+        // Redirect output to a client’s web browser (Excel5)
+        header('Content-Type: application/vnd.ms-excel');
+        header('Content-Disposition: attachment;filename="'.$today.'-form.xls"');
+        header('Cache-Control: max-age=0');
+        // If you're serving to IE 9, then the following may be needed
+        header('Cache-Control: max-age=1');
+
+        // If you're serving to IE over SSL, then the following may be needed
+        //header ('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
+        header ('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT'); // always modified
+        header ('Cache-Control: cache, must-revalidate'); // HTTP/1.1
+        header ('Pragma: public'); // HTTP/1.0
+
+        $objWriter = \PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
+        $objWriter->save('php://output');
 	}
 
 	print '
